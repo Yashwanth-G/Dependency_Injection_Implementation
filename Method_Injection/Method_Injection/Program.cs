a2e957eb2b3644aee9ea6595f7bb1bb5a7e945f0
Yashwanth Gundlapally <g.yashwanth896@gmail.com>
2022-09-23 11:50:54 -0400

    Method_Injection

---
+++ Method_Injection/Method_Injection/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public interface IWriter
+{
+    public void write();
+}
+
+public class DocumentWriter : IWriter
+{
+    public void write()
+    {
+        Console.WriteLine("Document Writer");
+    }
+}
+
+public class LetterWriter : IWriter
+{
+    public void write()
+    {
+        Console.WriteLine("Letter Writer");
+    }
+}
+
+
+public class SelectWriter
+{
+   public void SelectWriterType(IWriter writer)
+    {
+        writer.write();
+    }
+}
+
+public class Method_Injection
+{
+    static void Main(string[] args)
+    {
+        SelectWriter sw = new SelectWriter();
+        sw.SelectWriterType(new LetterWriter());
+    }
+}
