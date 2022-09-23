9dc57cc56d03fdeebffd8aa7612b2c28822d3558
Yashwanth Gundlapally <g.yashwanth896@gmail.com>
2022-09-23 11:45:40 -0400

    Setter_Injection

---
+++ Setter_Injection/Setter_Injection/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+public interface IWriter
+{
+    public void write();
+}
+
+public class DocumentWriter : IWriter
+{
+    public void write()
+    {
+        Console.WriteLine("Document Writer");
+    }
+}
+
+public class LetterWriter : IWriter
+{
+    public void write()
+    {
+        Console.WriteLine("Letter Writer");
+    }
+}
+
+
+public class SelectWriter
+{
+    private IWriter _writer;
+
+    public IWriter writer
+    {
+        set
+        {
+            _writer = value;
+        }
+        get
+        {
+            if (_writer == null)
+                throw new Exception("Object not passed to the property");
+            else
+                return _writer;
+        }
+    }
+}
+
+public class Setter_Injection
+{
+    static void Main(string[] args)
+    {
+        SelectWriter sw = new SelectWriter();
+        sw.writer = new DocumentWriter();
+        sw.writer.write();
+    }
+}
