3ddea97b5a5645e8ee6795c152b0d7ec4e9d5f81
Yashwanth Gundlapally <g.yashwanth896@gmail.com>
2022-09-23 11:56:28 -0400

    Constructor_Injection using C#

---
+++ Constructor_Injection_CSharp/Constructor_Injection_CSharp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public interface IWriter
+{
+    public void write();
+}
+
+public class DocumentWriter : IWriter
+{
+    public void write()
+    {
+        Console.WriteLine("Document Writer");
+    }
+}
+
+public class LetterWriter : IWriter
+{
+    public void write()
+    {
+        Console.WriteLine("Letter Writer");
+    }
+}
+
+
+public class SelectWriter
+{
+
+    private IWriter writer;
+    public SelectWriter(IWriter writer)
+    {
+        this.writer = writer;
+    }
+
+    public void DisplayWriter()
+    {
+        writer.write();
+    }
+}
+
+public class Constructor_Injection
+{
+    static void Main(string[] args)
+    {
+
+        // Dependency object is passed through the Constructor below.
+        SelectWriter sw = new SelectWriter(new LetterWriter());
+
+        sw.DisplayWriter();
+    }
+}
